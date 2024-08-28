8902ba29ea8ccb76b44f4e50c59b2848e2e1389f
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-08-28 05:27:08 -0700

    changes getLine.c

--- getLine.c
+++ getLine.c
@@ -10,38 +10,32 @@
  */
 ssize_t input_buffer(info_t *shell_info, char **buf, size_t *len)
 {
-	ssize_t r = 0;
-	size_t len_p = 0;
+	ssize_t bytes_read = 0;
+	size_t buffer_length = 0;
 
 	if (!*len) /* if nothing left in the buffer, fill it */
 	{
-		/*bfree((void **)info->cmd_buf);*/
 		free(*buf);
 		*buf = NULL;
 		signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-		r = getline(buf, &len_p, stdin);
+		bytes_read = getline(buf, &buffer_length, stdin);
 #else
-		r = _getline(shell_info, buf, &len_p);
+		bytes_read = _getline(shell_info, buf, &buffer_length);
 #endif
-		if (r > 0)
+		if (bytes_read > 0)
 		{
-			if ((*buf)[r - 1] == '\n')
+			if ((*buf)[bytes_read - 1] == '\n')
 			{
-				(*buf)[r - 1] = '\0'; /* remove trailing newline */
-				r--;
+				(*buf)[bytes_read - 1] = '\0'; /* remove trailing newline */
+				bytes_read--;
 			}
 			shell_info->linecount_flag = 1;
 			strip_comments(*buf);
 			append_history_list(shell_info, *buf, shell_info->histcount++);
-			/* if (_strchr(*buf, ';'))
-			{
-				*len = r;
-				shell_info->cmd_buf = buf;
-			} */
 		}
 	}
-	return (r);
+	return (bytes_read);
 }
 
 /**
@@ -52,41 +46,44 @@ ssize_t input_buffer(info_t *shell_info, char **buf, size_t *len)
  */
 ssize_t get_input(info_t *shell_info)
 {
-	static char *buf; /* the ';' command chain buffer */
+	static char *buffer; /* the ';' command chain buffer */
 	static size_t i, j, len;
-	ssize_t r = 0;
-	char **buf_p = &(shell_info->arg), *p;
+	ssize_t bytes_read = 0;
+	char **buffer_ptr = &(shell_info->arg), *current_position;
 
 	_putchar(BUF_FLUSH);
-	r = input_buffer(shell_info, &buf, &len);
-	if (r == -1) /* EOF */
+	bytes_read = input_buffer(shell_info, &buffer, &len);
+	if (bytes_read == -1) /* EOF */
 		return (-1);
 	if (len) /* we have commands left in the chain buffer */
 	{
 		j = i; /* init new iterator to current buf position */
-		p = buf + i; /* get pointer for return */
+		current_position = buffer + i; /* get pointer for return */
 
-		check_chain(shell_info, buf, &j, i, len);
+		check_chain(shell_info, buffer, &j, i, len);
 		while (j < len) /* iterate to semicolon or end */
 		{
-			if (is_chain(shell_info, buf, &j))
+			if (is_chain(shell_info, buffer, &j))
 				break;
 			j++;
 		}
 
-		i = j + 1; /* increment past nulled ';'' */
+		i = j + 1; /* increment past nulled ';' */
 		if (i >= len) /* reached end of buffer? */
 		{
 			i = len = 0; /* reset position and length */
 			shell_info->cmd_buf_type = CMD_NORM;
 		}
 
-		*buf_p = p; /* pass back pointer to current command position */
-		return (_strlen(p)); /* return length of current command */
+		/* pass back pointer to current command position */
+		*buffer_ptr = current_position;
+		/* return length of current command */
+		return (_strlen(current_position));
 	}
 
-	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
-	return (r); /* return length of buffer from _getline() */
+	/* else not a chain, pass back buffer from _getline() */
+	*buffer_ptr = buffer;
+	return (bytes_read); /* return length of buffer from _getline() */
 }
 
 /**
@@ -99,14 +96,14 @@ ssize_t get_input(info_t *shell_info)
  */
 ssize_t read_buffer(info_t *shell_info, char *buf, size_t *i)
 {
-	ssize_t r = 0;
+	ssize_t bytes_read = 0;
 
 	if (*i)
 		return (0);
-	r = read(shell_info->readfd, buf, READ_BUF_SIZE);
-	if (r >= 0)
-		*i = r;
-	return (r);
+	bytes_read = read(shell_info->readfd, buf, READ_BUF_SIZE);
+	if (bytes_read >= 0)
+		*i = bytes_read;
+	return (bytes_read);
 }
 
 /**
@@ -122,38 +119,38 @@ int _getline(info_t *shell_info, char **ptr, size_t *length)
 	static char buf[READ_BUF_SIZE];
 	static size_t i, len;
 	size_t k;
-	ssize_t r = 0, s = 0;
-	char *p = NULL, *new_p = NULL, *c;
+	ssize_t bytes_read = 0, total_bytes = 0;
+	char *p = NULL, *new_p = NULL, *newline_pos;
 
 	p = *ptr;
 	if (p && length)
-		s = *length;
+		total_bytes = *length;
 	if (i == len)
 		i = len = 0;
 
-	r = read_buf(shell_info, buf, &len);
-	if (r == -1 || (r == 0 && len == 0))
+	bytes_read = read_buffer(shell_info, buf, &len);
+	if (bytes_read == -1 || (bytes_read == 0 && len == 0))
 		return (-1);
 
-	c = _strchr(buf + i, '\n');
-	k = c ? 1 + (unsigned int)(c - buf) : len;
-	new_p = _realloc(p, s, s ? s + k : k + 1);
+	newline_pos = _strchr(buf + i, '\n');
+	k = newline_pos ? 1 + (unsigned int)(newline_pos - buf) : len;
+	new_p = _realloc(p, total_bytes, total_bytes ? total_bytes + k : k + 1);
 	if (!new_p) /* MALLOC FAILURE! */
 		return (p ? free(p), -1 : -1);
 
-	if (s)
+	if (total_bytes)
 		_strncat(new_p, buf + i, k - i);
 	else
 		_strncpy(new_p, buf + i, k - i + 1);
 
-	s += k - i;
+	total_bytes += k - i;
 	i = k;
 	p = new_p;
 
 	if (length)
-		*length = s;
+		*length = total_bytes;
 	*ptr = p;
-	return (s);
+	return (total_bytes);
 }
 
 /**
